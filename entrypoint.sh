d4b2048d9281f3024ddbd37bc8cfe6cac90d2737
Martin Kolek <kolek@modpreneur.com>
2017-03-20 17:09:37 +0100

    deploy files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 
-composer install --no-interaction
+composer dump-autoload --optimize --apcu
+composer run-script post-install-cmd --no-interaction
+
+bin/console cache:clear -n -e=prod
 
 chmod 0777 -R /var/app/app/cache
 chmod 0777 -R /var/app/app/logs
+
 
 exec apache2-foreground
